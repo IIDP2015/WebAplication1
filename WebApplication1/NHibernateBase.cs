8fc95cffd9bcc452944f045652dc9ba4e83a46c5
Szczepan Zaskalski <szczepix@goldrapid.pl>
2018-01-31 20:07:07 +0100

    add nhibernate config

---
+++ WebApplication1/NHibernateBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using Iesi;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+namespace BiuroPracy.BusinessLogic.NHibernate
+{
+    class NHibernateBase
+    {
+        public static Configuration configuration { get; set; }
+        public static ISession session { get; set; }
+
+        public static IStatelessSession statelessSession { get; set; }
+
+
+
+        protected static ISessionFactory SessionFactory { get; set; }
+
+
+        public static ISession Session => (session = SessionFactory.OpenSession());
+
+
+        public static IStatelessSession StatelessSession => (statelessSession = SessionFactory.OpenStatelessSession());
+
+
+        private static Configuration ConfigureRHibernate()
+        {
+            
+            try
+            {
+                configuration = new Configuration();
+                configuration.Configure();
+
+                return configuration;
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+           
+
+
+
+        }
+
+        public void Initialize()
+        {
+            configuration = ConfigureRHibernate();
+            SessionFactory = configuration.BuildSessionFactory();
+            new SchemaExport(configuration);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
